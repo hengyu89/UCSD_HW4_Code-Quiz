e88e929886983a322bb2c035bb7773a599192eec
Heng Yu <heng.yu161@gmail.com>
2022-04-15 00:40:23 -0700

    Add the problem set, need to conclude correct and take scores

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,6 +7,7 @@ startPage.hidden = false;
 questionPage.hidden = true;
 summaryPage.hidden = true;
 scorePage.hidden = true;
+var resultArea = document.querySelector('.result-area');
 
 // buttons (all submit and chooses buttons) and display data (like timer and score)
 var timerEl = document.querySelector('.timer-count');       // all pages: top right time left
@@ -16,18 +17,31 @@ var initialButton = document.querySelector('.submit-score');// 3rd page: submit 
 var scoreDisplay = document.querySelector('.total-score');  // 3rd page: Final score
 var initialInput = document.querySelector('#initial');      // 3rd page: input area
 var scoreList = document.querySelector('#score-list');      // 4th page: score list
+var goBackButton = document.querySelector('.go-back');      // 4th page: go back to main page
+var clearButton = document.querySelector('.reset-score');   // 4th page: clear all scores.
 
 // Values needed for this quiz.
 var timerCount;             // Time left
 var questionLeft;           // # of questions in Quiz
 var questionScore;          // credit of each question
-var totalScore = 80;       // Current score (100 initially)
+var totalScore = 100;       // Current score (100 initially)
 var scoreboard = [];   // All users data.
 var currentUser = {         // current users data
     initial: "",
     score: 0
 };
-console.log(currentUser);
+var questionSet = [
+    {Q: "This is question 1.",A1:"Answer 1",A2:"Answer 2",A3:"Answer 3",A4:"Answer 4"},
+    {Q: "This is question 2.",A1:"Answer 1",A2:"Answer 2",A3:"Answer 3",A4:"Answer 4"},
+    {Q: "This is question 3.",A1:"Answer 1",A2:"Answer 2",A3:"Answer 3",A4:"Answer 4"},
+    {Q: "This is question 4.",A1:"Answer 1",A2:"Answer 2",A3:"Answer 3",A4:"Answer 4"},
+    {Q: "This is question 5.",A1:"Answer 1",A2:"Answer 2",A3:"Answer 3",A4:"Answer 4"},
+    {Q: "This is question 6.",A1:"Answer 1",A2:"Answer 2",A3:"Answer 3",A4:"Answer 4"},
+    {Q: "This is question 7.",A1:"Answer 1",A2:"Answer 2",A3:"Answer 3",A4:"Answer 4"}
+];
+var currentQuestionSet = questionSet;
+
+
 function init() {
     var storedScoreboard = JSON.parse(localStorage.getItem("Scoreboard"));
     if(storedScoreboard !== null) {
@@ -41,7 +55,9 @@ function startQuiz() {
     timerCount = 60;
     questionLeft = 5;   // Number of questions this quiz  have.
     questionScore = totalScore/questionLeft; // how many credit each question worth
+    resultArea.hidden = true;
     // turn to question page right after click the start
+    chooseQuestion();
     startPage.hidden = true;
     questionPage.hidden = false;
     // start tik-tok.
@@ -75,18 +91,33 @@ function turnScorePage() {
 function nextQuestion() {
     // Turn to the page determined by how many questions left
     if (questionLeft > 1) {
-        questionPage.hidden = false;
+        questionPage.hidden = false;    // Go to question page
         summaryPage.hidden = true;
+        resultArea.hidden = false;      // check correction of previous one
+        chooseQuestion();
     } else {
         scoreDisplay.textContent = totalScore;
         questionPage.hidden = true;
-        summaryPage.hidden = false;
+        summaryPage.hidden = false;     // Go to summary page
         clearInterval(timer);
-        chooseQuestion();
     }
 
-    console.log(questionLeft);
     questionLeft --;
+}
+
+function chooseQuestion() {
+    var index = Math.floor(Math.random() * currentQuestionSet.length);
+    var answerSet = [currentQuestionSet[index].A1, currentQuestionSet[index].A2,currentQuestionSet[index].A3, currentQuestionSet[index].A4];
+    document.querySelector(".question-title").textContent = currentQuestionSet[index].Q;
+    document.querySelector(".answer-area").innerHTML = "";
+    for (var i = 0; i < 4; i++) {
+        var currentAnswer = answerSet[i];
+        var button = document.createElement("button");
+        button.textContent = i + ". " + currentAnswer;
+        button.setAttribute("class", "question-button");
+        document.querySelector(".answer-area").appendChild(button);
+    }
+    currentQuestionSet.splice(index,1);
 }
 
 // read the initial from user and turn to 4th page (scoreboard).
@@ -96,19 +127,21 @@ function toScoreboard() {
     if (currentUser.initial === "") {
         currentUser.initial = "anonymity";
     }
-    currentUser.score = Math.floor(totalScore);
-    console.log(currentUser);
-    scoreboard.push(currentUser);
-    console.log(scoreboard)
-    // test
-    console.log("before sort: "+JSON.stringify(scoreboard));
-    // finish test
-    scoreboard.sort(compareByScore);
-    // test
-    console.log("after sort: "+JSON.stringify(scoreboard));
-    console.log('length: '+scoreboard.length);
-    // finish test
 
+    currentUser.score = Math.floor(totalScore);
+    scoreboard.push(currentUser);
+    scoreboard.sort(sortByName);    // sort by name
+    scoreboard.sort(sortByScore);   // then sort by score
+
+    writeScoreboard();
+    storeScores();
+
+    summaryPage.hidden = true;
+    scorePage.hidden = false;
+    initialInput.value = "";
+}
+
+function writeScoreboard() {
     // Scoreboard stores 5 scores total.
     while(scoreboard.length > 5) {
         scoreboard.pop();
@@ -116,35 +149,61 @@ function toScoreboard() {
 
     scoreList.innerHTML = "";
     for (var i = 0; i < scoreboard.length; i++) {
-        var currentUserInfo = i + ". " + scoreboard[i].initial + " - " + scoreboard[i].score;
-        console.log(currentUserInfo);
+        var currentUserInfo = (i+1) + ". " + scoreboard[i].initial + " - " + scoreboard[i].score;
         var li = document.createElement("li");
         li.textContent = currentUserInfo;
         li.setAttribute("score-index",i);
         if (i%2 == 0) {
             li.classList.add("odd-list");
-            console.log("this works at:",i);
         }
         scoreList.appendChild(li);
 
     }
-    storeScores();
-
-    summaryPage.hidden = true;
-    scorePage.hidden = false;
 }
 
 function storeScores() {
     localStorage.setItem("Scoreboard", JSON.stringify(scoreboard));
 }
 
-function compareByScore(a,b) {
+function sortByName(a,b) {
+    const nameA = a.initial.toUpperCase();
+    const nameB = b.initial.toUpperCase();
+    if (nameA < nameB) {
+        return -1;
+    }
+    if (nameA > nameB) {
+        return 1;
+    }
+    return 0;
+}
+
+function sortByScore(a,b) {
     return b.score - a.score;
+}
+
+function goBack() {
+    startPage.hidden = false;
+    questionPage.hidden = true;
+    summaryPage.hidden = true;
+    scorePage.hidden = true;
+    init();
+}
+
+function clearScoreboard() {
+    scoreboard = [];
+    storeScores();
+    writeScoreboard();
+    var li = document.createElement("li");
+    li.textContent = "--";
+    li.classList.add("odd-list");
+    scoreList.appendChild(li);
 }
 
 
 startButton.addEventListener("click", startQuiz);
 questionButton.addEventListener("click", nextQuestion);
 initialButton.addEventListener("click", toScoreboard);
+goBackButton.addEventListener("click", goBack);
+clearButton.addEventListener("click", clearScoreboard);
 
 init();
